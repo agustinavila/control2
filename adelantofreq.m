e4146df678124556c61f709be69d19caedeb7e60
Agustin Avila <tinto.avila@gmail.com>
2020-12-03 12:39:46 -0300

    Creados compensadores frecuenciales

---
+++ adelantofreq.m
@@ -0,0 +1,11 @@
+function Gadf = adelantofreq(L,especificaciones)
+% Funcion que calcula un compensador de adelanto de fase para
+% especificaciones frecuenciales
+
+%Juan Agustin Avila
+%Diciembre 2020
+%Matlab r2020b
+
+%Aca deberia hacer algo xd
+
+end
---
+++ atrasofreq.m
@@ -0,0 +1,11 @@
+function Gatf = atrasofreq(L,especificaciones)
+% Funcion que calcula un compensador de atraso de fase para
+% especificaciones frecuenciales
+
+%Juan Agustin Avila
+%Diciembre 2020
+%Matlab r2020b
+
+%Aca deberia hacer algo xd
+
+end
