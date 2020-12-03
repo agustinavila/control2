a6c6a55b6cb21f6a89c8bd20172f55414b925d36
Agustin Avila <tinto.avila@gmail.com>
2020-12-03 12:26:13 -0300

    Corregido el codigo para cuando son necesarias varias etapas

--- adelantodefase.m
+++ adelantodefase.m
@@ -1,7 +1,7 @@
 function [compensador1,sd] = adelantodefase(planta,os,ts,sd)
-% [Gadelanto,sd]=ADELANTODEFASE(planta,os,ts,sd) 
+% [Gadelanto,sd]=ADELANTODEFASE(planta,os,ts,sd)
 % devuelve la funcion de transferencia de un compensador de adelanto
-% de fase con un oveshoot y tiempo de establecimiento dado, para 
+% de fase con un oveshoot y tiempo de establecimiento dado, para
 % la FTLA de la planta a controlar. sd es opcional para no utilizar los
 % calculos propios del programa.
 
@@ -10,23 +10,27 @@ function [compensador1,sd] = adelantodefase(planta,os,ts,sd)
 % Matlab r2020b
 if nargin < 4
     disp("no pusiste el punto sd");
-sd=puntosd(os,ts);
+    sd=puntosd(os,ts);
 end
 fase=180-phase(evalfr(planta,sd))*180/pi ;  %obtiene la ganancia de fase necesaria en el punto
 Gplanta=abs(evalfr(planta,sd));             %obtiene la ganancia en el punto
-try
-    [polo,cero]=polocero(sd,fase);              %obtiene polo y cero del compensador
-catch ME
-    if(strcmp(ME.identifier,'Polocero:Titamax'))
-        disp("El angulo a compensar es mayor a titamax, se utilizaran dos polos y dos ceros")
-        [polo,cero]=polocero(sd,fase/2);
-        polo=[polo polo];
-        cero=[cero cero];
+n=1;
+while 1
+    try
+        [polo,cero]=polocero(sd,fase/n,-3);              %obtiene polo y cero del compensador
+        break;
+    catch ME
+        if(strcmp(ME.identifier,'Polocero:Titamax'))
+            n=n+1;
+            disp("El angulo a compensar es mayor a titamax, se utilizaran "+n+" etapas")
+        end
     end
 end
 t=-1/cero(1) ;                                 %obtiene el t
 alfa=-1/(polo(1)*t);                           %obtiene el alfa
-Kc=abs((alfa*t*sd)+1)/(alfa*abs((t*sd)+1)*Gplanta); %obtiene el kc
-compensador1=zpk(cero,polo,Kc);             %genera la ft
+pz=zpk(cero*ones(1,n),polo*ones(1,n),1);
+gpz=abs(evalfr(pz,sd));
+Kc=abs(1/(gpz*Gplanta)); %obtiene el kc
+compensador1=pz*Kc;      %genera la ft
 end
 
